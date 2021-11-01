f38def82a3b4ba4772b190dda8202cc6a704b0f4
MihailAulov <titul7777@mail.ru>
2021-11-02 00:21:28 +0300

    add second programs add third programs

---
+++ src/main/java/3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class ThirdProgram {
+
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int i;
+        i = num.nextInt();
+        System.out.println("My variable is:" + i);
+    }
+}
---
+++ src/main/java/SecondProgram.java
@@ -0,0 +1,7 @@
+public class SecondProgram {
+    public static void main(String[] args) {
+        char sym = 'A';
+        System.out.println("Variable = " + sym);
+
+    }
+}
