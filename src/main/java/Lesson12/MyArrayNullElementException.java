b93b487ab51b7a31b6759a05d24987b6f9812b04
MihailAulov <titul7777@mail.ru>
2022-01-10 08:47:40 +0300

    HomeWork Lesson10

---
+++ src/main/java/Lesson12/MyArrayNullElementException.java
@@ -0,0 +1,8 @@
+package Lesson12.exception;
+
+public class MyArrayNullElementException extends RuntimeException {
+
+    public MyArrayNullElementException(String message) {
+        super(message);
+    }
+}
